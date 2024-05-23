8129ed49483cfca0c6811632280b39858708e66f
Felipe Martinez <lipericcimartinez22@gmail.com>
2024-05-23 17:18:54 -0300

    adicionando gráfico de dispersão e colocando scatter como parâmetro

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ if __name__ == '__main__':
     my_controller = controller.Controller()
     my_controller.preprocess_and_training()
     my_controller.calcular_e_salvar()
-    my_controller.view_model()
+    my_controller.view_model(matrix=True, scatter=True)
